32443b8a7bc154ece2dd519642851cabd76a5f37
João <joaovictorpfr@gmail.com>
2022-10-05 14:41:05 -0300

    code Js bugs

--- js/darkMode.js
+++ js/darkMode.js
@@ -3,4 +3,6 @@ inputDark.addEventListener("change", function turnOn(event){
     console.log("clickado");
     const $body = document.querySelector("body");
     $body.classList.toggle("turnOn");
+    const $input = document.querySelectorAll(".text");
+    $input.forEach()
 });
