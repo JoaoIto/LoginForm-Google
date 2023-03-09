734237c4a60e8ccca8af7b807151ca7563bb5462
João <joaovictorpfr@gmail.com>
2023-03-09 17:04:23 -0300

    Add page home

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { SignupForm } from './pages/SignUp'
+import { Home } from './pages/Home'
 import './assets/styles/App.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <SignupForm />
+    <Home />
   </React.StrictMode>,
 )
